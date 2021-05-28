385c593811bd21e3d7dfc379a3e970a88baff4e3
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-05-28 11:49:33 -0300

    rotas

--- Projeto/blogPessoal/src/app/app-routing.module.ts
+++ Projeto/blogPessoal/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
-  
+  {path:'entrar',component:EntrarComponent},
+  {path:'cadastrar',component:CadastrarComponent},
+  {path:'',redirectTo:'entrar',pathMatch:'full'}
 ];
 
 @NgModule({
