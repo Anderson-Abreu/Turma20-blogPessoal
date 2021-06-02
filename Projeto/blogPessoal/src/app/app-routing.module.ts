7d83d1a0562fa6df47d8844826e438e65c911b1b
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-06-02 11:21:37 -0300

    Tema estáticao

--- Projeto/blogPessoal/src/app/app-routing.module.ts
+++ Projeto/blogPessoal/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   {path:'entrar',component:EntrarComponent},
   {path:'cadastrar',component:CadastrarComponent},
   {path:'inicio', component:InicioComponent},
+  {path:'tema', component:TemaComponent},
   {path:'',redirectTo:'entrar',pathMatch:'full'}
 ];
 
